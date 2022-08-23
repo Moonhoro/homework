c8facbf21afa1ca73ee60b21b40e3cc229ad9b4b
Moonhoro <97378760+Moonhoro@users.noreply.github.com>
2022-08-23 11:05:58 +0800

    Add files via upload

---
+++ 7 WEEK/Solution.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            PriorityQueue<Integer>pq=new PriorityQueue<>();
+            for (int i=0;i<k;i++)pq.add(nums[i]);
+            for(int i=k;i<nums.length;i++)if(!pq.isEmpty()&&nums[i]>pq.peek()){
+                pq.remove();
+                pq.add(nums[i]);
+            }
+return pq.peek();
+        }
+    }
+
